3a815e4c03a4971fec91c91f41947fba5314a796
mariamelomaga <maria.melo.maga@gmail.com>
2021-08-05 17:57:52 +0100

    Added bootstrap + GIT link + Netlify link

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function Weather() {
-  return <div className="Weather">Weather app</div>;
+  return (
+    <div className="Weather">
+      <h1>Lisbon</h1>
+
+      <footer>
+        This project was coded by Maria Melo and {""}
+        <a
+          href="https://github.com/mariamelomaga/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+          alt="gitLink"
+        >
+          is open-sourced on GitHub {""}
+        </a>{" "}
+        and {""}
+        <a
+          href="https://festive-bose-62cc2d.netlify.app/"
+          target="_blank"
+          alt="netlifyLink"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
